1fd5b6ce2bb96d1d759b3d7faf3026c9705933f9
Rumi152 <90864146+Rumi152@users.noreply.github.com>
2023-11-06 15:19:44 +0100

    Implemented adding new films to db

--- FilmWebsite 06 11 2023/Rumi152/index.php
+++ FilmWebsite 06 11 2023/Rumi152/index.php
@@ -1,6 +1,29 @@
 <?php
 
 $conn = mysqli_connect("localhost", "root", "", "filmwebsite");
+
+if (isset($_POST["dodaj"])) {
+    $titleT = $_POST["title"];
+    $dateT = $_POST["date"];
+    $descriptionT = $_POST["description"];
+    $imageT = $_POST["image"];
+
+    $sql = "INSERT INTO `film`(
+        `Title`,
+        `ImageLink`,
+        `ProductionDate`,
+        `Description`
+    )
+    VALUES(
+        '$titleT',
+        '$imageT',
+        '$dateT',
+        '$descriptionT'
+    )";
+
+    mysqli_query($conn, $sql) or die("Problem z połączeniem");
+}
+
 $sql = "SELECT * FROM `film`;";
 $result = mysqli_query($conn, $sql) or die("Problem z połączeniem");
 
@@ -16,6 +39,22 @@ $result = mysqli_query($conn, $sql) or die("Problem z połączeniem");
 </head>
 
 <body>
+    <form method="POST">
+        <label for="title">Title: </label>
+        <input type="text" name="title" /><br>
+
+        <label for="date">Production date: </label>
+        <input type="date" name="date" /><br>
+
+        <label for="description">Description: </label>
+        <textarea type="text" name="description"></textarea><br>
+
+        <label for="image">Image link: </label>
+        <input type="text" name="image" /><br>
+
+        <input type="submit" name="dodaj" value="dodaj">
+    </form>
+
     <table>
         <?php
         while ($row = mysqli_fetch_row($result)) {
@@ -24,10 +63,10 @@ $result = mysqli_query($conn, $sql) or die("Problem z połączeniem");
             for ($i = 0; $i < 6; $i++) {
                 echo '<th style="padding: 0 2px">';
                 if ($i == 3)
-                    echo '<img src="'.$row[$i].'"></img>';
+                    echo '<img height="50px" src="' . $row[$i] . '"></img>';
                 else
                     echo $row[$i];
-                    
+
                 echo '</th>';
             }
 
